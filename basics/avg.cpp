9ce203733661c2bac100681f986b5918f61f03bb
SnigdhaMarkanday <59870580+SnigdhaMarkanday@users.noreply.github.com>
2020-04-27 23:33:43 +1300

    avg of two no.

--- basics/avg.cpp
+++ basics/avg.cpp
@@ -2,10 +2,12 @@
 using namespace std;
 int main()
 {
-  int n1,n2,s;
+  int n1,n2,s,a;
   cout<<"enter 2 no.";
   cin>>n1>>n2;
   s=n1+n2;
+  a=s/2;
   cout<<"sum of"<<n1<<"and"<<n2<<" is"<<s;
+  cout<<"avrg of "<<n1 <<"and"<<n2<<"is"<<a;
  return 0;
 }
