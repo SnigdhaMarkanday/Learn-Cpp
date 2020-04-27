a4ad904f5144ac4563979376f5c2c99de86c85f7
SnigdhaMarkanday <59870580+SnigdhaMarkanday@users.noreply.github.com>
2020-04-28 03:20:09 +1300

    int main function added

--- basics/marks.cpp
+++ basics/marks.cpp
@@ -1,9 +1,11 @@
 #include<iostream>
 using namespace std;
+int main()
 {
     int m,p,c;
     m=90;
     p=77;
     c=69;
     cout<<"Maths="<<m<<"\nPhysics="<<p<<"\nChemistry="<<c;
+    return 0;
 }
