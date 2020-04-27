29acf07d530a028c7a2609a62cfd34873f0fe571
SnigdhaMarkanday <59870580+SnigdhaMarkanday@users.noreply.github.com>
2020-04-28 02:14:04 +1300

    simple algebra equation

---
+++ basics/algebra.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int x,a,b,c;
+  cout<<"Enter the value of a,b and c";
+  cin>>a>>b>>c;
+  x=a/(b-c);
+  cout<<"value of x is"<<x;
+  return 0;
+}
