dbabdc64505426bd140f1f94ebda74758fbb5f90
SnigdhaMarkanday <59870580+SnigdhaMarkanday@users.noreply.github.com>
2020-04-27 23:30:07 +1300

    average of two no.s

---
+++ basics/avg.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int n1,n2,s;
+  cout<<"enter 2 no.";
+  cin>>n1>>n2;
+  s=n1+n2;
+  cout<<"sum of"<<n1<<"and"<<n2<<" is"<<s;
+ return 0;
+}
