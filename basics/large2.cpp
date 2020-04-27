3ccecbc5252023be772ff22a09a4072686bccb4d
SnigdhaMarkanday <59870580+SnigdhaMarkanday@users.noreply.github.com>
2020-04-28 02:06:37 +1300

    To check which of the two no.s is larger

---
+++ basics/large2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int n1,n2;
+  cout<<"Enter 2 no.s";
+  cin>>n1>>n2;
+  if(n1>n2)
+  {
+    cout<<n1<<"is larger;
+  }
+  else
+  {
+    cout<<n2<<"is larger;
+  }
+  return 0;
+}  
