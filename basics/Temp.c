f192e9e79a229d0826c1eccf61225ed8fa3436ee
SnigdhaMarkanday <59870580+SnigdhaMarkanday@users.noreply.github.com>
2020-04-28 02:18:11 +1300

    temperature conversion from farehneit to celsius

---
+++ basics/Temp.c
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int f,c;
+    cout<<"Enter temp in Farehneit ";
+    cin>>f;
+    c=(5/9*f)-32;
+    cout<<"Temp in Celsius is:"<<c<<"degree C";
+    return 0;
+}
+    
