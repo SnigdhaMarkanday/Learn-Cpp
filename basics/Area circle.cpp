6e3d9528aee58f2285080476e9e4ee0805ecd711
SnigdhaMarkanday <59870580+SnigdhaMarkanday@users.noreply.github.com>
2020-05-06 22:39:18 +1300

    area of circle

---
+++ basics/Area circle.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int a,r;
+  cout<<"Enter the radius of circle";
+  cin>>r;
+  a=(3.14*r*r);
+  cout<<"Area of Circle is"<<a;
+  return 0;
+}
