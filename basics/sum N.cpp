40e7bc3b60a5ba0ce3ee6a2d54cecf8b6e7258a7
SnigdhaMarkanday <59870580+SnigdhaMarkanday@users.noreply.github.com>
2020-05-06 20:33:01 +1300

    Sum of n natural no.s

---
+++ basics/sum N.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int n,s;
+  cout<<"How many natural no.s are used";
+  cin>>n;
+  s=(n*(n-1))/2;
+  cout<<"sum of "<<n<<"natural no.s is"<<s;
+  return 0;
+ }
+ 
