331e2f6d3a83bd7e3f0f25d86dce308ff5c4dce2
SnigdhaMarkanday <59870580+SnigdhaMarkanday@users.noreply.github.com>
2020-05-06 23:07:10 +1300

    Create SI.cpp

---
+++ basics/SI.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int p,t;
+  float r,si;
+  cout<<"Enter the values of p,r,t";
+  cin>>p>>r>>t;
+  si=(p*r*t)/100;
+  cout<<"Simple Interest is :"<<si;
+  return 0;
+ }
