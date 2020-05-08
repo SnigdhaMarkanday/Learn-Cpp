a43aaa5c0e3eb4e6903bc8b73ed2b5c99fbd6017
SnigdhaMarkanday <59870580+SnigdhaMarkanday@users.noreply.github.com>
2020-05-09 03:51:03 +1300

    Program for checking whether its working hours or leisure hours

---
+++ Conditional Statements/working hrs.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int h;
+  cout<<"Enter the hours";
+  cin>>h;
+  if(h>=9 && h<=18){
+    cout<<"Working hours";
+   }
+   else{
+    cout<<"Leisure";
+   }
+   return 0;
+}
+  
