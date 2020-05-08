1771a0641ad3b11625e4202444f8d72e05dca3f3
SnigdhaMarkanday <59870580+SnigdhaMarkanday@users.noreply.github.com>
2020-05-08 22:24:46 +1300

    check for positive and negative numbers

---
+++ Conditional Statements/positive negative.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int n;
+  cout<<"Enter a no.";
+  cin>>n;
+  if(n>0){
+    cout<<n<<"is Positive no.";
+   }
+   else{
+    if(n=0){
+      cout<<"Zero";
+     }
+     else{
+      cout<<"Negative";
+     }
+   }
+   return 0;
+ }
+ 
