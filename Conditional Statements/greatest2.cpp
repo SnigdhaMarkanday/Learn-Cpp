8baecaa273b591a00e8b1420e41a10650f84f1b2
SnigdhaMarkanday <59870580+SnigdhaMarkanday@users.noreply.github.com>
2020-05-08 22:15:07 +1300

    Greatest of 2 no.s

---
+++ Conditional Statements/greatest2.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespaces std;
+int main(){
+  int n1,n2;
+  cout<<"Enter the values";
+  ci>>n1>>n2;
+  if(n1>n2){
+    cout<<n1<<"is greatest";
+   }
+   else{
+    cout<<n2<<"is greater";
+   }
+   return 0;
+}
