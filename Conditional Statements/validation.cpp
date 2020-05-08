0315c5ce6642d36d8b19045cce9738fdfdf9a116
SnigdhaMarkanday <59870580+SnigdhaMarkanday@users.noreply.github.com>
2020-05-09 01:15:48 +1300

    for checking validation of no.s

---
+++ Conditional Statements/validation.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int n1,n2;
+  cout<<"Enter the no.s to be divide";
+  cin>>n1>>n2;
+  if(n1%n2!=0){
+    cout<<"not valid";
+   }
+   else{
+    cout<<"valid";
+   }
+   return 0;
+}
