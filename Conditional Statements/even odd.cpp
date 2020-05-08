cffb6ced6a4d8ea2eff8a1b8f7a096be80ad6c69
SnigdhaMarkanday <59870580+SnigdhaMarkanday@users.noreply.github.com>
2020-05-08 22:59:54 +1300

    To check whether the no. is even or odd

---
+++ Conditional Statements/even odd.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int n;
+  cout<<"Enter no.:;
+  cin>>n;
+  if(n%2==0){
+    cout<<"Even no.";
+   }
+   else{
+    cout<<"Odd no.";
+   }
+   return 0;
+ }
+ 
