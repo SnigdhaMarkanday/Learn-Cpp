1fc1248b0e36d0f9e267c36c910f5f88f0b08c2d
SnigdhaMarkanday <59870580+SnigdhaMarkanday@users.noreply.github.com>
2020-05-09 23:44:56 +1300

    Whether the person is eligible for the offer or not

---
+++ Conditional Statements/Eligible.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int a;
+  cout<<"Enter the age";
+  cin>>a;
+  if(a<=12 || a>=50){
+    cout<<"Eligible";
+   }
+   else{
+    cout<<"Not Eligible";
+   }
+   return 0;
+ }
+ 
