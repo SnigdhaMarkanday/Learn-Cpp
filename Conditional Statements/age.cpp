f2cd1f6b7364ce701bbe2de77b59b0cdb8c72e35
SnigdhaMarkanday <59870580+SnigdhaMarkanday@users.noreply.github.com>
2020-05-09 23:42:18 +1300

    to check whether the person is young or not young

---
+++ Conditional Statements/age.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int a;
+  cout<<"Enter the age of the person";
+  cin>>a;
+  if(a>=12 && a<=50){
+    cout<<"Young";
+   }
+   else{
+    cout<<"Not Young";
+   }
+   return 0;
+ }
+ 
