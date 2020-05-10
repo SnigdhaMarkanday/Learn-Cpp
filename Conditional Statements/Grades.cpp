4c3b22e3987ec562e2e987a7b9ac96d6c20eebf4
SnigdhaMarkanday <59870580+SnigdhaMarkanday@users.noreply.github.com>
2020-05-11 10:32:27 +1300

    Grades of student

---
+++ Conditional Statements/Grades.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int e,m,h,t,p;
+  cout<<"Enter marks";
+  cin>>e>>m>>h;
+  t=e+m+h;
+  p=(t/3)*100;
+  if(p>=60){
+    cout<<"A Grade";
+   }
+   else{
+    if((p<=35)||(p<60)){
+      cout<<"B Grade";
+     }
+     else{
+      cout<<"C Grade";
+     }
+    }
+    return 0;
+}
+
+  
+  
+  
