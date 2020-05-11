a563d21265c3925c898a909123165a5344ca8654
SnigdhaMarkanday <59870580+SnigdhaMarkanday@users.noreply.github.com>
2020-05-12 00:09:33 +1300

    Create Month.cpp

---
+++ Conditional Statements/Month.cpp
@@ -0,0 +1,82 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int m;
+  cout<<"Enter the no.";
+  cin>>m;
+  if(m==1){
+    cout<<"January";
+   }
+   else{
+    if(m==2){
+      cout<<"Feburary";
+     }
+     else{
+      if(m==3){
+        cout<<"March";
+       }
+       else{
+        if(m==4){
+          cout<<"April";
+         }
+         else{
+          if(m==5){
+            cout<<"May";
+          }
+          else{
+            if(m==6){
+              cout<<"June";
+             }
+             else{
+              if(m==7){
+                cout<<"July";
+               }
+               else{
+                if(m==8){
+                  cout<<"August";
+                 }
+                 else{
+                  if(m==9){
+                    cout<<"September";
+                   }
+                   else{
+                    if(m==10){
+                      cout<<"October";
+                     }
+                     else{
+                      if(m==11){
+                        cout<<"November";
+                       }
+                       else{
+                        if(m==12){
+                          cout<<"December";
+                         }
+                         else{
+                          cout<<"Wrong Input";
+                         }
+                       }
+                      }
+                     }
+                    }
+                   }
+                  }
+                 }
+                }
+               }
+              }
+             }
+            }
+           }
+          }
+         }
+        }
+       }
+      }
+     }
+    }
+   }
+  }
+ }
+}
+
+             
