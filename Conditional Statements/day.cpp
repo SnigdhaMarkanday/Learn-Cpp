64034f3bbaf16a488352f6227b2fa3c9a7f3e995
SnigdhaMarkanday <59870580+SnigdhaMarkanday@users.noreply.github.com>
2020-05-12 00:01:04 +1300

    Enter no. to see the day at that no.

---
+++ Conditional Statements/day.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int d;
+  cout<<"Enter the no.";
+  cin>>d;
+  if(d==1){
+    cout<<"Monday";
+   }
+   else{
+    if(d==2){
+      cout<<"Tuesday";
+     }
+     else{
+      if(d==3){
+        cout<<"Wednusday";
+       }
+       else{
+        if(d==4){
+          cout<<"Thursday";
+        }
+        else{
+          if(d==5){
+            cout<<"Friday";
+           }
+           else{
+            if(d==6){
+              cout<<"Saturday";
+            }
+            else{
+              if(d==7){
+                cout<<"Sunday";
+              }
+              else{
+                cout<<"Weong Input";
+               }
+            }
+          }
+        }
+     }
+   }
+ }
+}
+}
