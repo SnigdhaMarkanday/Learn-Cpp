591b38ae6882d68e9a8c610b3e1f0ab01a867f34
SnigdhaMarkanday <59870580+SnigdhaMarkanday@users.noreply.github.com>
2020-05-12 05:44:53 +1300

    Switch Case for day

---
+++ Conditional Statements/Switch day.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int d;
+  cout<<"Enter the day no.";
+  cin>>d;
+  switch(d){
+    case 1:
+        cout<<"Monday";
+        break;
+   case 2 :
+        cout<<"Tuesday";
+        break;
+  case 3:
+        cout<<"Wednusday";
+        break;
+  case 4:
+        cout<<"Thursday";
+        break;
+ case 5:
+        cout<<"Friday";
+        break;
+ case 6:
+      cout<<"Saturday";
+      break;
+ case 7:
+      cout<<"Sunday";
+      break;
+ default:
+     cout<<"Wrong Input";
+  }
+}
