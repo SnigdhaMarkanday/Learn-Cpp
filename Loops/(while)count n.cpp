79f725038ba87b83444068ee89702a4a4c82be7f
SnigdhaMarkanday <59870580+SnigdhaMarkanday@users.noreply.github.com>
2020-05-12 20:38:53 +1300

    Create (while)count n.cpp

---
+++ Loops/(while)count n.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int n;
+  cout<<"Enter the value of n";
+  cin>>n;
+  i=1;
+  while(i<=n){
+    cout<<i;
+    i++;
+   }
+   return 0 ;
+}
+
+   
