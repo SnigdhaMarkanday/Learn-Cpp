e5e0cf319018784ded82bfb5e8c5fc8dc30debbb
SnigdhaMarkanday <59870580+SnigdhaMarkanday@users.noreply.github.com>
2020-05-12 23:20:15 +1300

    Table of any no.

---
+++ Loops/Table.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int n,a;
+  cout<<"Enter the no.";
+  cin>>n;
+  for(int i=0;i<=10;i++){
+    a=(n*i);
+    cout<<n<<"*"<<i<<"="<<a;
+   }
+   return 0;
+}
+    
