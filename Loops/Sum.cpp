69476637b862a5016725cb81ad8f73fe6a5eebe5
SnigdhaMarkanday <59870580+SnigdhaMarkanday@users.noreply.github.com>
2020-05-12 23:26:11 +1300

    Sum of n no.s

---
+++ Loops/Sum.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int n,s=0;
+  cout<<"Enter no. upto which u need sum";
+  cin>>n;
+  for(i=1;i<=n;i++){
+    s=s+i;
+   }
+   cout<<"Sum is"<<s;
+  return 0;
+}
