281f771b6dcb4af494fe81e6dcf38f26b6e5f960
SnigdhaMarkanday <59870580+SnigdhaMarkanday@users.noreply.github.com>
2020-05-12 23:43:42 +1300

    Factorial of no.s

---
+++ Loops/Factorial.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int i,f,n;
+  f=1;
+  cout<"Enter the value of n";
+  cin>>n;
+  for(i=1;i<=n;i++){
+    f=f*i;
+   }
+   cout<<"Factorial is "<<f;
+   return 0;
+}
+  
