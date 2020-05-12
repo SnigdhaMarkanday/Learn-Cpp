6e57fadf348360fce8579c520f05e2c767107367
SnigdhaMarkanday <59870580+SnigdhaMarkanday@users.noreply.github.com>
2020-05-12 23:59:09 +1300

    Create Factors.cpp

---
+++ Loops/Factors.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int n,i;
+  cout<<"Enter the no. whose Factors u need";
+	cin>>n;
+	for(i=n;i>=0;i--){
+		if(n%i==){
+			cout<<i;
+		}
+	}
+	return 0;
+}
