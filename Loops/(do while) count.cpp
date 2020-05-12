f25618f0b14bd2c76ff7e8886d341c8efd6f4c9e
SnigdhaMarkanday <59870580+SnigdhaMarkanday@users.noreply.github.com>
2020-05-12 20:42:55 +1300

    using do while count

---
+++ Loops/(do while) count.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int n;
+  cout<<"Enter a no.";
+  cin>>n;
+  int i;
+  i=1;
+  do{
+   cout<<i;
+   i++;
+  }while(i<=n);
+  return 0;
+ }
+ 
+    
