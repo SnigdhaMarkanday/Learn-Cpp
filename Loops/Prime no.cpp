184e0113504e370ea371ab354b369b09a169831a
SnigdhaMarkanday <59870580+SnigdhaMarkanday@users.noreply.github.com>
2020-05-13 04:11:56 +1300

    Prime no.

---
+++ Loops/Prime no.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int n;
+  cout<<"Enter the no.";
+  cin>>n;
+  int i;
+  if((n%n==0) && (n%1==0)){
+    for(i=1;i<=n;i++){
+      if(n%i!=0){
+        cout<<"Prime no";
+       }
+       else{
+        cout<<"Not Prime";
+       }
+      }
+      return 0;
+}
+     
+      
+  
