cad437432198a663d293822f35c1a6c7bf59fe5a
SnigdhaMarkanday <59870580+SnigdhaMarkanday@users.noreply.github.com>
2020-05-14 07:01:01 +1300

    Linear Search program

---
+++ Array/Linear Search.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using nameaspace std;
+int main(){
+  int A[10],i,k;
+  cou<<"Enter no.s";
+  for(i=0;i<10;i++){
+    cin>>A[i];
+   }
+   cout<<"Enter the key";
+   cin>>k;
+   for(i=0;i<10;i++){
+    if(k==A[i]){
+     cout<<"Found at"<<i;
+     return 0;
+     }
+     }
+     cout<<"Not Found";
+     return 0;
+ }
+    
+     
+    
+   
+   
+   
+   
+    
