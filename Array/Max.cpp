10d418ca125d1511ff55af02908e315177931f9d
SnigdhaMarkanday <59870580+SnigdhaMarkanday@users.noreply.github.com>
2020-05-13 22:45:42 +1300

    Program to find maximum of the elements of the array

---
+++ Array/Max.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int i,A[6],k;
+  A[6]={2,3,4,6,7,10};
+  for(i=0;i<6;i++){
+    cout<<A[6];
+   }
+   k=A[0];
+   for(i=1;i<6;i++){
+    	if(A[i]>k){
+				k=A[i];
+			}
+		}
+		cout<<k;
+		return 0;
+	}
+   
