1bfba13750345bf08fdd12b6fd37869694ef7eb6
SnigdhaMarkanday <59870580+SnigdhaMarkanday@users.noreply.github.com>
2020-05-13 22:20:16 +1300

    Sum of Arrays

---
+++ Array/Sum.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int A[6],i,s=0;
+  A[6]={2,3,4,5,6,7};
+  for(i=0;i<6;i++){
+    s=s+A[i];
+   }
+   cout<<"Sum is:"<<s;
+   return 0;
+}
+    
+  
+  
+  
