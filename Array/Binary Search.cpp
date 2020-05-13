da903f9f26df589b46b806331f43dc9163e0afa2
SnigdhaMarkanday <59870580+SnigdhaMarkanday@users.noreply.github.com>
2020-05-14 09:27:41 +1300

    Binary Search Prgm

---
+++ Array/Binary Search.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int i,m,l,h,A[10],k;
+  A[6]={3,4,5,7,9,14};
+  cout<<"Enter the key";
+  cin>>k;
+  l=0;
+  h=5;
+  m=(l+h)/2;
+  for(i=0;i<6;i++){
+    
+    
+    
+    
+  
