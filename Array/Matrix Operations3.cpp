e6bdd6aea86f62a5adc5373d9292d12789f7d873
SnigdhaMarkanday <59870580+SnigdhaMarkanday@users.noreply.github.com>
2020-05-16 06:32:45 +1300

    Avrg of the two matrices

---
+++ Array/Matrix Operations3.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int A[2][3]={3,3,3,3,3};
+  int B[2][3]={1,1,1,1,1};
+  int C[2][3];
+  for(int i=0;i<2;i++){
+    for(int j=0;j<3;j++){
+      C[i][j]=(A[i][j]+B[i][j])/2;
+      cout<<C[i][j]<<" ";
+      }
+      cout<<endl;
+   }
+   return 0;
+}
+   
