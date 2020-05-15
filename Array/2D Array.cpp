398586e2e8886279c0e90e4148d2b13be004b236
SnigdhaMarkanday <59870580+SnigdhaMarkanday@users.noreply.github.com>
2020-05-15 23:31:19 +1300

    Created a multi dimentional Array

---
+++ Array/2D Array.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int A[2][3] = {{2,3,4},{3,6,5}};
+  int i,j;
+  for(i=0;i<2;i++){
+    for(j=0;j<3;j++){
+      cout<<A[2][3];
+     }
+     cout<<endl;
+   }
+   return 0;
+ }
+   
