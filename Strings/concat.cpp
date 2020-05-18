8e32613445dc65041613b3f075ee0974eb7e9d77
SnigdhaMarkanday <59870580+SnigdhaMarkanday@users.noreply.github.com>
2020-05-18 22:04:29 +1300

    string concatination function

---
+++ Strings/concat.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+int main(){
+  char s1[20];
+  char s2[30];
+  strcat(s1,s2);   // strncat(destination,source,length)
+  cout<<s1;
+  return 0;
+ }
+  
