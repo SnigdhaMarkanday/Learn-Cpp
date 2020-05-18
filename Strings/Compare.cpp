0d011a14900c6988a2659459b3dc4d0df7bfbcd1
SnigdhaMarkanday <59870580+SnigdhaMarkanday@users.noreply.github.com>
2020-05-18 23:20:45 +1300

    compare function in strings

---
+++ Strings/Compare.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+int main(){
+  char s1[50]="Hello";
+  char s2[50]="hello";
+  cout<<strcmp(s1,s2);
+  return 0;
+ }//h ASCii code is greater than H so s1 is greater than s2
+  
