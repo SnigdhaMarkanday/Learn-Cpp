beb4d3a4942a29ca0212705868e96311f2b4e319
SnigdhaMarkanday <59870580+SnigdhaMarkanday@users.noreply.github.com>
2020-05-18 23:38:05 +1300

    Create Tokeniser.cpp

---
+++ Strings/Tokeniser.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+int main(){
+  char s1[50="235";
+  char s2[50]="59.768";
+  long int x=strtol(s1,NULL,10);
+  float y=strtof(s2,NULL);
+  cout<<x<<endl<<y<<endl;
+  return 0;
+ }
