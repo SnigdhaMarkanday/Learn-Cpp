871cd134f77690695cc8f84f235c388e5d437eaa
SnigdhaMarkanday <59870580+SnigdhaMarkanday@users.noreply.github.com>
2020-05-19 01:09:58 +1300

    Append class string function

---
+++ Strings/Append.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+  string str="Hello";
+  str.append(" World"); //append means add at the end
+  cout<<str;
+  return 0;
+ }
+  
