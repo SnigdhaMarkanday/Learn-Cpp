48f3339fb0933e93c29af8e8e7a5cbda44a1ce24
SnigdhaMarkanday <59870580+SnigdhaMarkanday@users.noreply.github.com>
2020-05-19 20:52:11 +1300

    Operators of class string

---
+++ Strings/Operators.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string>
+uing namespace std;
+int main(){
+  string s1="programming";
+  string s2="Hello";
+  string s3;
+  cout<<s.at(4); //can also write s[4] and can rewrite also 
+  s3=s2+s1; // used for concatenation
+  cout<<s3;
+  return 0;
+ }
+  
+  
