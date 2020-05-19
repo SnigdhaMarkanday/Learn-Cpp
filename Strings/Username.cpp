8b4403db09de66f3f3b28354918b2b5a08f32451
SnigdhaMarkanday <59870580+SnigdhaMarkanday@users.noreply.github.com>
2020-05-19 23:40:37 +1300

    To find the user name from email address

---
+++ Strings/Username.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+  string m="snigiimarkanday@gmail.com";
+  cout<<"Username is:";
+  for(i=0;m[i]!='@';i++){
+    cout<<m[i];
+   }
+   return 0;
+ }
+
+ 
+    
+    
