4954880c2cd431adaaaa0a004b838f94ab92941f
SnigdhaMarkanday <59870580+SnigdhaMarkanday@users.noreply.github.com>
2020-05-19 22:41:45 +1300

    Iterator class string function

---
+++ Strings/Iterator.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+  string s="Today";
+  string ::iterator it;
+  for(it=s.begin();it!=s.end;i++){
+    cout<<*it;
+    }
+    return 0;
+   }
