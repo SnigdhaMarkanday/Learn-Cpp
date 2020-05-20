62f33b9a2ecb5c2fd6c456abcc48cad048f1d763
SnigdhaMarkanday <59870580+SnigdhaMarkanday@users.noreply.github.com>
2020-05-20 20:46:42 +1300

    function named add and main

---
+++ Functions/Add.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+suing namespace std;
+int add (int x, int y){
+  z=x+y;
+  return z;
+ }
+ int main(){
+  int a,b,s;
+  cout<<"Enter the values of a and b";
+  cin>>a>>b;
+  s=add(a,b);
+  cout<<"Sum is :"<<s;
+  return 0;
+ }
+  
+ 
