bc81045c8aeb04082d7867a0309f2e3e7379b4a1
SnigdhaMarkanday <59870580+SnigdhaMarkanday@users.noreply.github.com>
2020-05-20 21:05:25 +1300

    Calculating the power of a no.

---
+++ Functions/Power.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int power(int x, int y){
+  for(int i=1;i<=y;i++){
+    int z=1;
+    z=(z*x);
+    }
+   }
+   return z;
+ }
+ int main(){
+  int m,n;
+  cout<<"Enter the no. and its power";
+  cin>>m>>n;
+  p=power(m,n);
+  cout<<"Value is :"<<p;
+  return 0;
+ }
+    
