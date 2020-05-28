ff6c910303a1b57cc1f1a8d2e4617a6e111b86e5
SnigdhaMarkanday <59870580+SnigdhaMarkanday@users.noreply.github.com>
2020-05-28 23:35:58 +1300

    Volume of cuboid

--- Inheritance/Example 2.cpp
+++ Inheritance/Example 2.cpp
@@ -5,21 +5,45 @@ private:
      int l;
      int b;
 public:
-  
- };
+     Rectangle();
+     Rectangle(int l, int b){
+         this->l=l;
+         this->b=b;
+     };
+     Rectangle(Rectangle &r);
+     int getl(){
+          return l;
+     }
+     int getb(){
+          return b;
+     }
+     void setlength(int l);
+     void setbreadth(int b);
+     int area();
+     int perimeter();
+   };
  class Cuboid :public Rectangle{
     private:
-        int h;
- };
+        int height;
+    public:
+         Cuboid (int l=0, int b=0,int h=0){
+              height=h;
+         }
+         int getheight(){return h;}
+         int setheight(int h){height=h;
+         }
+         int volume(){
+              return (getl()*getb()*h);
+         }
+   };
  int main(){
-       Rectangle r;
-       r.l=67;
-       r.b=44;
-       r.area();
-       Cuboid c;
-       c.l=5;
-       c.b=8;
-       c.h=4;
-       c.area();
+       Cuboid c(4);
+       c.setl(5);
+       c.setb(8);
+      cout<<"Volume is"<<c.volume()<<endl;
+      return 0;
+ }
+ 
+       
        
         
