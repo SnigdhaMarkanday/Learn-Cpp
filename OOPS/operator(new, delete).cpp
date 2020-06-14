d3cd0843a3a8bb4cfc508347678742a7a33cfbc0
SnigdhaMarkanday <59870580+SnigdhaMarkanday@users.noreply.github.com>
2020-06-14 22:15:51 +1300

    usage of operators such as new and delete

---
+++ OOPS/operator(new, delete).cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+void main(){
+    int *p;
+    int s;
+    cout<<"Enter the size of integer";
+    cin>>s;
+    cout<<"Creating an array of size"<<s;
+    p=new int[s];
+   cout<<"\n"<<Dynamic Allocation of array";
+   delete p;
+ }
